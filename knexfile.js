37aa72937bcaf9ab4e383158c7ec5df915a9c0a9
Chris Jordan <cjorda15@msudenver.edu>
2017-07-14 23:27:14 -0600

    maybe look into the knexfile might help (sigh) changed production connection to pg

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/police',
+    connection: process.env.DATABASE_URL+ `?ssl=true` || 'postgres://localhost/police',
     migrations: {
       directory: './db/migrations'
     },
